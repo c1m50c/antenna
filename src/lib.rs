47c8fb60f7a2f293bde9723f3fe6b776d3103e51
c1m50c <58411864+c1m50c@users.noreply.github.com>
2023-12-19 19:13:04 -0600

    Add `tree-sitter` + Implement `RecognizedLanguage` enumerator

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,50 @@
+use tree_sitter::Language;
 
+/// An enumerator holding variants that are languages _recognized_ by `antenna`.
+#[derive(Debug, PartialEq)]
+pub enum RecognizedLanguage {
+    #[cfg(feature = "rust")]
+    Rust,
+}
+
+impl RecognizedLanguage {
+    /// Converts the [`RecognizedLanguage`] to a Tree Sitter [`Language`].
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use antenna::RecognizedLanguage;
+    ///
+    /// assert_eq!(
+    ///     RecognizedLanguage::Rust.as_tree_sitter_language(),
+    ///     tree_sitter_rust::language()
+    /// );
+    /// ```
+    pub fn as_tree_sitter_language(&self) -> Language {
+        match self {
+            #[cfg(feature = "rust")]
+            Self::Rust => tree_sitter_rust::language(),
+        }
+    }
+
+    /// Attempts to construct a [`RecognizedLanguage`] from a language's file extension.
+    /// If the extension is not mappable to a _[`RecognizedLanguage`]_, this function will return [`None`].
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use antenna::RecognizedLanguage;
+    ///
+    /// assert_eq!(
+    ///     RecognizedLanguage::from_language_extension("rs"),
+    ///     Some(RecognizedLanguage::Rust)
+    /// );
+    /// ```
+    pub fn from_language_extension(extension: &str) -> Option<Self> {
+        match extension {
+            #[cfg(feature = "rust")]
+            "rs" => Some(Self::Rust),
+            _ => None,
+        }
+    }
+}
