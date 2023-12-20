7f323774cf34c63f71c52b99ece3597005b255f9
c1m50c <58411864+c1m50c@users.noreply.github.com>
2023-12-20 17:13:23 -0600

    Basic `clap` argument parsing

---
+++ src/args.rs
@@ -0,0 +1,15 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+pub struct AntennaArguments {
+    /// Path to the `antenna` settings file.
+    #[arg(
+        short,
+        long,
+        env = "ANTENNA_SETTINGS_FILE",
+        default_value = "./antenna.yml"
+    )]
+    pub settings_file: PathBuf,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,25 @@
 use std::fs;
 
 use antenna::{configuration::AntennaConfiguration, AntennaResult};
+use clap::Parser;
+
+mod args;
 
 fn main() -> AntennaResult<()> {
-    let configuration = fs::read_to_string("./antenna.yml")?;
-    let configuration = serde_yaml::from_str::<AntennaConfiguration>(&configuration)?;
+    let arguments = args::AntennaArguments::parse();
 
-    configuration
-        .queries
-        .iter()
-        .try_for_each(|x| x.run(rayon::ThreadPoolBuilder::new().num_threads(4).build().unwrap()))?;
+    let configuration = serde_yaml::from_str::<AntennaConfiguration>(&fs::read_to_string(
+        &arguments.settings_file,
+    )?)?;
+
+    configuration.queries.iter().try_for_each(|x| {
+        x.run(
+            rayon::ThreadPoolBuilder::new()
+                .num_threads(4)
+                .build()
+                .unwrap(),
+        )
+    })?;
 
     Ok(())
 }
