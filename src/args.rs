b6bf1500694e1598e2e403c562c9278e97d3c172
c1m50c <58411864+c1m50c@users.noreply.github.com>
2023-12-20 23:19:31 -0600

    Spice up CLI `--help` information

--- src/args.rs
+++ src/args.rs
@@ -3,12 +3,13 @@ use std::path::PathBuf;
 use clap::Parser;
 
 #[derive(Debug, Parser)]
+#[command(author, version, about, long_about = None, help_template = "{name} ({version})\n{about-section}{author-section}\n{usage-heading} {usage}\n\n{all-args}")]
 pub struct AntennaArguments {
-    /// Path to the `antenna` settings file.
+    /// Path to the `antenna` configuration file.
     #[arg(
         short,
         long,
-        env = "ANTENNA_SETTINGS_FILE",
+        env = "ANTENNA_CONFIGURATION_FILE",
         default_value = "./antenna.yml"
     )]
     pub configuration_file: PathBuf,
